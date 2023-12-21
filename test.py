ac154c9b40bd3fdba579088aea5c71fa3d18a4fb
secrey <gidbrn@mail.ru>
2023-12-22 05:43:19 +0700

    itpa4

--- test.py
+++ test.py
@@ -1,10 +1,33 @@
-def suggest_places(a, b):
-  """Returns nihuya
+def hex_interpreter(h):
+    h = h.lstrip('#')
+    return list(int(h[i:i + 2], 16) for i in (0, 2, 4))
 
-  :param a: var a nahui
-  :type a: str
-  :param b: textual input
-  :type b: str
 
-  :rtype: (str, str), str, str
-  :return: (nuhyyaaaa), super api func"""
+def rgb_interpreter(rgb):
+    return '#%02x%02x%02x' % rgb
+
+def lighten(h, percent):
+    color = hex_interpreter(h)
+    goal_1 = color[0] + (255 - color[0]) * percent // 100
+    goal_2 = color[1] + (255 - color[1]) * percent // 100
+    goal_3 = color[2] + (255 - color[2]) * percent // 100
+    print(color)
+    print(goal_1, goal_2, goal_3)
+    color = (goal_1, goal_2, goal_3)
+    return rgb_interpreter(color)
+
+def darken(h, percent):
+    color = hex_interpreter(h)
+    goal_1 = color[0] * percent // 100
+    goal_2 = color[1] * percent // 100
+    goal_3 = color[2] * percent // 100
+    print(color)
+    print(goal_1, goal_2, goal_3)
+    color = (goal_1, goal_2, goal_3)
+    return rgb_interpreter(color)
+
+
+
+print(darken('#000000', 100))
+print(lighten('#99CCFF', 50))
+print(darken('#45A1F0', 20))
